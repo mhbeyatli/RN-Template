48f806f148177f9003609ee3795b460950dde282
Habib <habibbeyatli@gmail.com>
2019-12-09 12:01:42 +0300

    Saga is implemented

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './src/reducers'
 import FirstPage from './src/FirstPage'
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './src/saga';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga)
 
 const App = () => {
   return (
